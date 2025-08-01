c937ad0c89ec0d97caa9a3d9965fedbc27bc1ba3
tc-as-43 <technetium43.as@gmail.com>
2025-08-01 16:05:38 +0600

    Update range_hash-cpp.cpp

--- range_hash-cpp.cpp
+++ range_hash-cpp.cpp
@@ -1,51 +1,11 @@
 int b = 43;
 int MOD= 1e9+7;
-// 1e9+7 | 1e9+9 | 1000004329 | 1000075057 
-// 1000111103 | 1763922631 | 1977326753 
-// 2122331213 | 3149403637 | 5769213841
+/* 1e9+7 | 1e9+9 | 1000004329 | 1000075057 
+   1000111103 | 1763922631 | 1977326753 
+   2122331213 | 3149403637 | 5769213841 */
  
-inline int binpow(int a, int b) {
-    int res = 1;
-    while (b > 0) {
-        if (b & 1) res *= a;
-        a *= a;
-        b >>= 1;
-    }
-    return res;
-}
 
-inline int modpow(int a, int b, int m = MOD) {
-    int res = 1; a %= m;
-    while (b > 0) {
-        if (b & 1) res = (res * a) % m;
-        a = (a * a) % m; b >>= 1;
-    } return res;
-}
-
-inline int modinv(int b, int m = MOD) {
-    
-    return modpow(b, m - 2, m);
-}
-
-inline int modadd(int a, int b)
-{
-    return ((a%MOD)+(b%MOD))%MOD;
-}
-
-inline int modsub(int a, int b)
-{
-    return ((a%MOD)-(b%MOD)+MOD)%MOD;
-}
-
-inline int modmul(int a, int b)
-{
-    return ((a%MOD)*(b%MOD))%MOD;
-}
-
-inline int moddiv(int a, int b, int m = MOD) {
-    return (a % m * modinv(b, m)) % m;
-}
-
+//bigmod function here....
 
 int returnhash(string s, int base=b, int m=MOD)
 {
